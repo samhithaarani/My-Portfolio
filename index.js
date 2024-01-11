d6e461c66f36aa9d3b312725d0e4627d7b4ddf41
samhithaarani <n180441@rguktn.ac.in>
2024-01-11 21:35:42 +0530

    simple-text-styles

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
     var projectData = [
       {
         title: 'CancerCareConnect',
-        text: '<p><strong>CancerCareConnect</strong> is made for a client to handle info about lung metastasis.  can upload and share details like names, primary tumor info, metastasis count, and CT images securely. The site has a private login system for the surgeon to manage content. It also shows a pie chart with the types of primary tumors.</p><strong>Technologies Used:</strong><p>HTML, CSS, JavaScript,Python,Django, and SQL</p>',
+        text: '<p><strong>CancerCareConnect</strong> is made for a client <br> to handle info about lung metastasis.<br>can upload and share details <br>like names, primary tumor info, <br>metastasis count, and CT images securely. <br>The site has a private login system <br>for the surgeon to manage content. <br>It also shows a pie chart <br> with the types of primary tumors.<br></p><strong>Technologies Used:</strong><p>HTML, CSS, JavaScript,Python,Django, <br>and SQL</p>',
        
      
         image: ['images/pr3.png'],
@@ -35,7 +35,7 @@ $(document).ready(function(){
         ]
       }, {
         title: 'Netflix-Clone',
-        text: '<p>This Application Provides a beautiful interface to users to know about recent movie trends,and provides a brief description about world’s best movies,as well users are able to see movie trailers of different genres.Tmdb API isused to get information about different movies.</p> <strong>Technologies Used:</strong>ReactJS<br> ',
+        text: '<p>This Application Provides a beautiful <br> interface to users to know about recent<br> movie trends,and provides a brief <br> descriptionabout world’s best <br> movies,as well users<br> are able to see movie trailers of <br>different genres.Tmdb API isused to <br>get information about different movies.</p> <strong>Technologies Used:</strong>ReactJS<br> ',
   
         image: ['https://i.pinimg.com/564x/79/d6/c0/79d6c0ea1a3f4fc704f2c35539671c5a.jpg'],
         site: [
@@ -44,7 +44,7 @@ $(document).ready(function(){
         ]
       }, {
         title: 'TODO APP',
-        text: '<p>Simple<strong> TodoList</strong> app which helps us to create , edit and delete todoitems along with the description.</p><strong>Technologies Used:</strong><p>ReactJs</p>',
+        text: '<p>Simple<strong> TodoList</strong> app which helps us <br>to create ,edit and delete <br> todoitems along with the description.</p><strong>Technologies Used:</strong><p>ReactJs</p>',
        
         image: ['images/pr1.png'],
         site: [
@@ -64,14 +64,14 @@ $(document).ready(function(){
        */
       {
         title: 'Search Buddy',
-        text: '<p><strong>Search Buddy </strong>Provides an interface for users to search anything and it shows the best relevant Results based onusers choice, this app is simply the blueprint of the American Ad Agency Craigslist.org. and uses the interesting and powerful web scraping module BeautifulSoup.<br><br><br><strong>Technologies Used:</strong>HTML,CSS,Django</p>',
+        text: '<p><strong>Search Buddy </strong>Provides an interface <br>for users to search anything <br>and it shows the best relevant<br> Results based onusers choice, <br>this app is simply the <br>blueprint of the American<br> Ad Agency Craigslist.org. and <br>uses the interesting and <br>powerful web scraping <br>module BeautifulSoup.<br><strong>Technologies Used:</strong>HTML,CSS,Django</p>',
         image: ['images/pr4.png'],
         site: [
           ['Source-code', 'fa-folder', 'https://github.com/samhithaarani/searchbuddy']
         ]
       }, {
         title: 'CMS',
-        text: '<p><strong>Content Management System</strong> is an app for students to get study materials. Admins share content with specific branches. Students log in to access what they need, and admins log in to manage resources. Simple and easy!</p>.<br><br><strong>Technologies Used :</strong> HTML,CSS,JS,PHP,SQL',
+        text: '<p><strong>Content Management System</strong> is an app for <br>students to get study materials.<br> Admins share content with<br> specific branches. Students log in to <br>access what they need, <br>and admins log in to manage resources.<br></p>.<br><strong>Technologies Used :</strong> HTML,CSS,<br>JS,PHP,SQL',
         image: ['images/pr5.png'],
         site: [
           ['Source-Code', 'fa-folder', 'https://github.com/samhithaarani/Content-Management-System']
@@ -91,7 +91,7 @@ $(document).ready(function(){
       */
       {
         title: '<small>Brain Tumor Classification(FSL)</small>',
-        text: 'We made a smart system to find brain tumors using special techniques. By teaching it with brain tumor data, it can tell two types of tumors apart. This helps doctors make better treatment choices, potentially improving patient health.<br><br><br><strong>Technologies Used:</strong>Deep Learning',
+        text: 'We made a smart system to find brain<br> tumors using special techniques. By <br>teaching it with brain tumor data, <br>it can tell two types of tumors apart.<br> This helps doctors make better <br>treatment choices, potentially<br> improving patient health.<br><strong>Technologies Used:</strong>Deep Learning',
         image: ['https://i.pinimg.com/736x/5b/18/96/5b1896043afdf90b34ab20c3637a10f3.jpg'],
         site: [
           ['Source-Code', 'fa-folder', 'https://colab.research.google.com/drive/1vnVUmAMR_tbskg5Ae-8-UpcJPknw-Hhy#scrollTo=M7l3qzY-d2AT']
